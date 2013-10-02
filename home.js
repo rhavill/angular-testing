a9abca95224d3bc764cbec5744b3b664bf21ee00
Ricardo Havill <rhavill@progresstesting.com>
2013-10-02 01:26:33 -0400

    first commit

---
+++ home.js
@@ -0,0 +1,28 @@
+'use strict';
+ /*
+var app = angular.module('MyApp', ['ngResource']);
+ 
+app.factory('UserFactory', function($resource){
+    return $resource('Users/users.json')
+});
+ 
+app.controller('MainCtrl', function($scope, UserFactory) {
+    $scope.text = 'Hello World!';
+    $scope.users = UserFactory.get();
+});
+
+*/
+var myApp = angular.module('myApp', []);
+
+myApp.factory('UserFactory', function () {
+  return { message: "I'm data from a service" };
+});
+
+// function FirstCtrl($scope, UserFactory) {
+//   $scope.data = UserFactory;
+//   $scope.text = 'Hello World!';
+// }
+myApp.controller('FirstCtrl', function($scope, UserFactory) {
+    $scope.text = 'Hello World!';
+    $scope.data = UserFactory;
+});
